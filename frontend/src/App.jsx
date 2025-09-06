ddff30ee9c60a8f7b8488e979818afa70226a086
Harsh Desai <desaiharsh512@gmail.com>
2025-09-06 21:01:16 +0530

    feat: Frontend initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {Home} from './pages/Home'
+import { Register } from './pages/Register'
+import { Login } from './pages/Login'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App

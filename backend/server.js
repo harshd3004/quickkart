6f28e13da5010067e5829233462d883371b1a178
Harsh Desai <desaiharsh512@gmail.com>
2026-01-08 23:01:39 +0530

    fetch data from backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,15 @@ const app = express();
 require('dotenv').config();
 const connectDB = require('./config/db');
 
+//cors
+const cors = require('cors');
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true
+}
+app.use(cors(corsOptions));
+
 //db connection
 connectDB();
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,33 @@
+import { use, useEffect, useState } from "react"
+import { getAllProducts } from "../api/productApi"
 export const Home = ()=>{
+
+    const [products, setProducts] = useState([]);
+    const [productCount, setProductCount] = useState(0);
+
+    useEffect(()=>{
+        const fetchData = async()=>{
+            try{
+                const res = await getAllProducts();
+                console.log("fetched");
+                console.log(res);
+                
+                
+                if(res){
+                    setProducts(res.data.products);
+                    setProductCount(res.data.total);
+                }
+            }
+            catch(err){
+                console.log("Error fetching products:", err);
+            }
+        };
+        fetchData();
+    },[])
     return (
-        <h1>Home Page</h1>
+        <>
+            <h1>Home Page</h1>
+            <h2>Total Products: {productCount}</h2>
+        </>
     )
 }
